abb79cc29d0e43322d3ca07ad46b10502cf30335
MiguelEXE <miguellencina13@gmail.com>
2021-02-21 19:35:45 -0300

    Added examples

--- files/boot/cm_init.js
+++ files/boot/cm_init.js
@@ -4,6 +4,7 @@ module.exports.startup = (shutdown1) => {
     console.log(chalk.green("Cmdoska INIT v1.0")+"\n");
     console.log(chalk.green("> Starting services"));
     require("fs").readdirSync("./files/etc/services").forEach(function(srv){
+        if(srv.includes(".notdead")) return;
         let service = require(`../etc/services/${srv}`);
         service.name ? undefined : service.name = srv.replace(/\.js/, "");
         console.log(chalk.blue("--> ")+chalk.green(`Starting ${service.name}...`));
@@ -21,6 +22,7 @@ module.exports.shutdown = () => {
     if(!require("fs").existsSync("./files/etc/services")) return "Cant get services folder";
     console.log(chalk.blue("> Stopping Services"));
     require("fs").readdirSync("./files/etc/services").forEach(function(srv){
+        if(srv.includes(".notdead")) return;
         let service = require(`../etc/services/${srv}`);
         return service.stop ? (function(){
             service.name ? undefined : service.name = srv.replace(/\.js/, "");
